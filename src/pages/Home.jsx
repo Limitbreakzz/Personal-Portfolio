895800e8325766b1634117a525dc72b71496940a
ICE-Lomphong <lomphongsrathongmee@gmail.com>
2026-04-23 09:56:24 +0700

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -15,7 +15,7 @@ const HomePage = () => {
           {/* รูปภาพตรงกลาง */}
           <div className="md:w-1/3 flex justify-center p-4 z-10 h-full">
             <img
-              src="/picture/me.png"
+              src="https://drive.google.com/u/0/drive-viewer/AKGpihbdnD4TpbI8MJPleLTb0HOePaJnNnsbtS0UzSh_HvVPGVbJ9b8caBBIIyzLAkLlVbi2wGTgcUYHwKDj1Stuke3O3RPBZPdXjw=s1600-rw-v1?auditContext=forDisplay"
               alt="me"
               className="w-full h-full max-w-lg object-cover"
             />
